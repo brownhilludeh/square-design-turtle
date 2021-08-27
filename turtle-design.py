2ca12e773c43fa07b4ba606abffc1fc485c23261
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2021-08-27 07:49:03 +0100

    Create turtle-design.py

---
+++ turtle-design.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+pensize(3)
+pencolor('brown')
+def logo():
+    for i in range(4):
+        forward(250)
+        left(90)
+        forward(100)
+        left(90)
+        forward(100)
+        left(90)
+
+
+
+logo()
+hideturtle()
+mainloop()
